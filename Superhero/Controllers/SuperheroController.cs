4fc78b20f2e330edea27af74bede3294a6306d97
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 15:52:26 -0600

    adding constructor to superhero controller

--- Superhero/Controllers/SuperheroController.cs
+++ Superhero/Controllers/SuperheroController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Superhero.Data;
 
 namespace Superhero.Controllers
 {
     public class SuperheroController : Controller
     {
         public ApplicationDbContext context;
+        public SuperheroController(ApplicationDbContext db)
+        {
+            context = db;
+        }
         public IActionResult Index()
         {
             return View();
