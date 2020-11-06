edd47c82d878fc41a990ffdb388cceeb6ab76bbd
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 15:48:47 -0600

    created superhero model logic

--- Superhero/Models/Superhero.cs
+++ Superhero/Models/Superhero.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Superhero.Models
 {
     public class Superhero
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Alterego { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string catchphrase { get; set; }
     }
 }
