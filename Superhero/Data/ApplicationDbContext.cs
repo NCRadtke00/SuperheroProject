28d46aa901046ad123ea508c0cdbb5f6fccd48d5
Nicholas Radtke <Raddnc@gmail.com>
2020-11-10 12:13:39 -0600

    hard seeded 3heros

--- Superhero/Data/ApplicationDbContext.cs
+++ Superhero/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Superhero.Models;
-
+using Microsoft.EntityFrameworkCore.Metadata;
 namespace Superhero.Data
 {
     public class ApplicationDbContext : IdentityDbContext
@@ -22,6 +22,40 @@ namespace Superhero.Data
         //        {
         //    });
         //}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
+            builder.Entity<Superheros>()
+                .HasData(
+                    new Superheros()
+                    {
+                        Id = 1,
+                        Name = "Superman",
+                        Alterego = "Clark Kent, Karl-El",
+                        PrimaryAbility = "Indomitable Will",
+                        SecondaryAbility = "Kryptonian Physiology, flight, strength, speed, heat vision, frost breath, healing",
+                        Catchphrase = "Up, Up, and Away!"
+                    },
+                    new Superheros()
+                    {
+                        Id = 2,
+                        Name = "Batman",
+                        Alterego = "Bruce Wayne",
+                        PrimaryAbility = "Billionaire Genius",
+                        SecondaryAbility = "Mastery of body and mind",
+                        Catchphrase = "Pow"
+                    },
+                    new Superheros()
+                    {
+                        Id = 3,
+                        Name = "Aquaman",
+                        Alterego = "Arthur Curry, King Orin",
+                        PrimaryAbility = "Atlantean Adaptation, Strength, Super-water speed, underwater breathing",
+                        SecondaryAbility = "​Enhanced Durability",
+                        Catchphrase = "By the beard of Poseidon!"
+                    });
+        
+        }
     }
 }
